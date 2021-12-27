1312a9b150aa86ac410489b545dcc91f879f96f0
nik.volkov <nikita.volkov@jetbrains.com>
2021-12-27 17:04:26 +0300

    some changes

--- src/components/Home/MainView.js
+++ src/components/Home/MainView.js
@@ -38,7 +38,7 @@ const GlobalFeedTab = React.memo((props) => {
         className={props.tab === "all" ? "nav-link active" : "nav-link"}
         onClick={clickHandler}
       >
-        Global Feed Testing
+        Global Feed Test
       </button>
     </li>
   );
